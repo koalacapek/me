24d4e38a24c4e53546457c2178155c23f1754a52
Hans Kristian Reynaldi <hanskr7882@gmail.com>
2025-05-27 19:36:44 +1000

    enable transition

--- next.config.ts
+++ next.config.ts
@@ -5,21 +5,17 @@ import createMDX from '@next/mdx'
 const nextConfig: NextConfig = {
   /* config options here */
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  experimental: {
+    viewTransition: true,
+  },
 
 };
 
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
   extension: /\.(md|mdx)$/,
-  options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [],
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
-  },
+
+
 })
 
 // Merge MDX config with Next.js config
