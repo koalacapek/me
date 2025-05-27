7374b00a64df0c4a373938c448a43e272e6127c9
Hans Kristian Reynaldi <hanskr7882@gmail.com>
2025-05-27 19:36:10 +1000

    create a new section, journey

---
+++ src/app/journey/page.tsx
@@ -0,0 +1,17 @@
+import type { MDXComponents } from "mdx/types";
+import { useMDXComponents } from "../../../mdx-components";
+import CustomLink from "@/components/CustomLink";
+
+export default async function Page() {
+  const { default: MDXContent } = await import("@/content/journey/index.mdx");
+  const components = useMDXComponents({
+    CustomLink,
+  } as MDXComponents);
+
+  return (
+    <article className="prose prose-stone dark:prose-invert relative">
+      <h1 className="font-bold text-xl">my journey</h1>
+      <MDXContent components={components} />
+    </article>
+  );
+}
