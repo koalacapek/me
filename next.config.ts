dcc854fc794d2aaf80e14d1915cdd20e72c12205
Hans Kristian Reynaldi <hanskr7882@gmail.com>
2025-05-28 20:20:18 +1000

    set default route to /about and create component to display skill and post

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,15 @@ const nextConfig: NextConfig = {
   experimental: {
     viewTransition: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/about',
+        permanent: true,
+      },
+    ];
+  },
 
 };
 
---
+++ src/components/BadgeGroup.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Badge } from "./ui/badge";
+
+interface BadgeGroupProps {
+  tags: string[];
+}
+
+export default function BadgeGroup({ tags }: BadgeGroupProps) {
+  return (
+    <div className="my-2 flex flex-row flex-wrap gap-2">
+      {tags.map((tag) => (
+        <Badge
+          key={tag}
+          className="border- rounded-full px-1.5 py-0.5 text-[10px] font-normal transition-transform select-none hover:scale-105"
+          variant="secondary"
+        >
+          {tag}
+        </Badge>
+      ))}
+    </div>
+  );
+}
