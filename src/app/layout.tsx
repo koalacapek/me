170eb76227e1883b9ed91f09acea5099851ab9c6
Hans Kristian Reynaldi <hanskr7882@gmail.com>
2025-06-07 21:24:15 +1000

    fix padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
       >
         <ViewTransition>
           <main className="h-fit max-h-screen">
-            <div className="relative mx-auto h-fit w-screen [&>article]:mx-auto px-8 pt-10 pb-10 md:max-w-md md:pb-24 md:pt-24 lg:max-w-xl lg:pb-24 xl:max-w-2xl xl:pb-24">
+            <div className="relative mx-auto h-fit w-screen [&>article]:mx-auto px-10 pt-10 pb-10 md:max-w-md md:pb-24 md:pt-24 lg:max-w-xl lg:pb-24 xl:max-w-2xl xl:pb-24">
               <Navbar />
               {children}
             </div>
